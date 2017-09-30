e5318d5413fcbddc2a592d38147040f0f0e6754d
letelete <brunokawka@gmail.com>
2017-09-30 22:27:50 +0200

    Update

--- Fontly/mainwindow.cpp
+++ Fontly/mainwindow.cpp
@@ -1,6 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QScrollArea>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QFontDatabase>
+#include <QString>
+#include <QSize>
+#include <QLine>
+#include <QColorDialog>
+#include <QDesktopServices>
+#include <QUrl>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
